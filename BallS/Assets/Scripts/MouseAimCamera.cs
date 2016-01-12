46dac48379c863fee7bd0976f4087f0167a87177
SilentTremor <eugeneatschool@gmail.com>
2016-01-12 17:27:26 -0600

    Testing different camera control methods.

---
+++ BallS/Assets/Scripts/MouseAimCamera.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MouseAimCamera : MonoBehaviour
+{
+    public GameObject Player;
+    public float damping = 1;
+    Vector3 offset;
+
+    void Start()
+    {
+        offset = transform.position - Player.transform.position;
+    }
+
+    void LateUpdate()
+    {
+        Vector3 desiredPosition = Player.transform.position + offset;
+        Vector3 position = Vector3.Lerp(transform.position, desiredPosition, Time.deltaTime * damping);
+        transform.position = position;
+
+        transform.LookAt(Player.transform.position);
+    }
+}
