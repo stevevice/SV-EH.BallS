bdfcc700bc126d34290fc12d94c7c7346c95f1e5
SilentTremor <eugeneatschool@gmail.com>
2016-01-12 15:31:10 -0600

    Included jumping and camera tracking. Expanded level.

--- BallS/Assets/Scripts/PlayerControler.cs
+++ BallS/Assets/Scripts/PlayerControler.cs
@@ -8,6 +8,8 @@ public class PlayerControler : MonoBehaviour
 
     private Rigidbody rb;
 
+    bool FloorTouch;
+
     void Start()
     {
         rb = GetComponent<Rigidbody>();
@@ -18,8 +20,22 @@ public class PlayerControler : MonoBehaviour
         float moveHorizontal = Input.GetAxis("Horizontal");
         float moveVertical = Input.GetAxis("Vertical");
 
+        if (Input.GetKeyDown("space") && FloorTouch == true)
+        {
+            transform.Translate(Vector3.up * 260 * Time.deltaTime, Space.World);
+            FloorTouch = false;
+        }
+
         Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
 
         rb.AddForce(movement * speed);
+    }
+
+    void OnCollisionEnter(Collision Other)
+    {
+        if (Other.gameObject.name == "Floor")
+        {
+            FloorTouch = true;
+        }
     }
 }
